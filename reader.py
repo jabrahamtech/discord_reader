525edc46607ebbb3478def74a5ee82f6d81b83c1
Jonathan <your>
2021-12-02 03:07:04 +1300

    initial commit

---
+++ reader.py
@@ -0,0 +1,57 @@
+import websocket
+import json
+import threading
+import time
+
+def send_json_request(ws, request):
+  ws.send(json.dumps(request))
+
+def recieve_json_response(ws):
+  response = ws.recv()
+  if response:
+    return json.loads(response)
+
+def heartbeat(interval, ws):
+  print('heartbeat begin')
+  while True:
+    time.sleep(interval)
+    heartbeatJSON ={
+      "op": 1,
+      "d": "null"
+    }
+    send_json_request(ws, heartbeatJSON)
+    print('Heartbeat sent')
+
+ws = websocket.WebSocket()
+ws.connect("wss://gateway.discord.gg/?v=6&encording=json")
+event = recieve_json_response(ws)
+heartbeat_interval = event['d']['heartbeat_interval'] / 1000
+threading._start_new_thread(heartbeat, (heartbeat_interval,ws))
+
+token = "OTAyNDk1ODQ4Mzg5MzAwMjg0.YXujMA.qNI5ZiSoQas8bggGdeT-mjrHIOA"
+payload = {
+  "op": 2,
+  "d":{
+    "token" : token,
+    "intents": 513,
+    "properties": {
+      "$os": 'linux',
+        '$browser': 'chrome',
+        '$device': 'pc'
+    }
+
+  }
+}
+
+send_json_request(ws, payload)
+
+while True:
+  event = recieve_json_response(ws)
+  try: 
+    content = event['d']['content']
+    print(f"{content}")
+    op_code = event('op')
+    if op_code == 11:
+      print('heatbeat received')
+  except:
+    pass
